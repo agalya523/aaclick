03ca6df9b56e8f4b5970b873712f2ba1b54591ca
agalya523 <dreamfairy523@gmail.com>
2024-12-21 20:17:26 +0530

    ADD

---
+++ script.js
@@ -0,0 +1,10 @@
+function abfunction(){
+document.getElementById('a').innerHTML="ABISHEK";
+}
+
+function absfunction(){
+    document.getElementById('c').innerHTML="&#128151";
+    
+    document.getElementById('b').innerHTML="AGALYA";
+    }
+
